0233461ca9021d8d370a59eb744de426e683284b
AShukilovich <AShukilovich@gmail.com>
2017-11-24 18:54:31 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var swiperhome = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+});
